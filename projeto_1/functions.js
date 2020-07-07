78363264f9cfe28c930bc6aa190bb9cbf7766548
Jonathan Silva | Cappta <jonathan.silva@cappta>
2020-07-07 14:45:31 -0300

    Project 1 Complete

---
+++ projeto_1/functions.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+const path = require('path')
+
+const readDirectory = pathDir => {
+  const archives = fs.readdirSync(pathDir)
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(archives.map(archive => path.join(pathDir, archive)))
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+const elementsEndsWith = filter => array => array.filter(el => el.endsWith(filter))
+
+const readArchive = pathArchive => new Promise((resolve, reject) => {
+  try {
+    resolve(fs.readFileSync(pathArchive, { encoding: 'utf-8' }).toString())
+  } catch (err) {
+    reject(err)
+  }
+})
+
+const readArchives = pathArchives =>
+  Promise.all(pathArchives.map(path => readArchive(path)))
+
+const removeIfEmpty = array => array.filter(el => el.trim())
+
+const removeIfIncludes = text => array => array.filter(el => !el.includes(text))
+
+const removeIfOnlyNumber = array => array.filter(el => isNaN(parseInt(el)))
+
+const removeCharacters = symbols =>
+  array => array.map(el => symbols.reduce((acc, symbol) =>
+    acc.split(symbol).join(''), el))
+
+const joinElements = array => array.join(' ')
+
+const splitTextBy = symbol => array => array.split(symbol)
+
+const agroupElements = elements =>
+  Object.values(elements.reduce((agroup, element) => {
+    const elementLower = element.toLowerCase()
+    const qtde = agroup[elementLower] ? agroup[elementLower].qtde + 1 : 1
+    agroup[elementLower] = { element: elementLower, qtde }
+    return agroup
+  }, {}))
+
+const sortElementByAttrNumber = (attr, order = 'asc') => array => {
+  const asc = (a, b) => a[attr] - b[attr]
+  const desc = (a, b) => b[attr] - a[attr]
+
+  return array.sort(order === 'asc' ? asc : desc)
+
+}
+
+module.exports = {
+  readDirectory,
+  elementsEndsWith,
+  readArchives,
+  readArchive,
+  removeIfEmpty,
+  removeIfIncludes,
+  removeIfOnlyNumber,
+  removeCharacters,
+  joinElements,
+  splitTextBy,
+  agroupElements,
+  sortElementByAttrNumber
+}
---
+++ projeto_1/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fn = require('./functions')
+
+const pathDir = path.join(__dirname, '..', 'legendas')
+
+const symbols = [
+  '.', '?', '-', ',', '"', '♪',
+  '_', '<i>', '</i>', '\r', '[',
+  ']', '(', ')', '!'
+]
+
+fn.readDirectory(pathDir)
+  .then(fn.elementsEndsWith('.srt'))
+  .then(fn.readArchives)
+  .then(fn.joinElements)
+  .then(fn.splitTextBy('\n'))
+  .then(fn.removeIfEmpty)
+  .then(fn.removeIfIncludes('-->'))
+  .then(fn.removeIfOnlyNumber)
+  .then(fn.removeIfIncludes('<font'))
+  .then(fn.removeCharacters(symbols))
+  .then(fn.joinElements)
+  .then(fn.splitTextBy(' '))
+  .then(fn.removeIfEmpty)
+  .then(fn.removeIfOnlyNumber)
+  .then(fn.agroupElements)
+  .then(fn.sortElementByAttrNumber('qtde', 'desc'))
+  .then(console.log)
